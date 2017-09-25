815ecf4bd5ab38d8d519e3216618ea5ccd5ebc1c
Daniel MF <echo.dmf@gmail.com>
2017-09-25 15:31:37 -0300

    some threejs work

--- build/reality.js
+++ build/reality.js
@@ -1440,6 +1440,30 @@ var units = Object.freeze({
 	        super(...arguments);
 	    }
 
+	    set scene(scene) {
+	        this._scene = scene;
+	    }
+
+	    get scene() {
+	        return this._scene;
+	    }
+
+	    set camera(camera) {
+	        this._camera = camera;
+	    }
+
+	    get camera() {
+	        return this._camera;
+	    }
+
+	    set renderer(renderer) {
+	        this._renderer = renderer;
+	    }
+
+	    get renderer() {
+	        return this._renderer;
+	    }
+
 	    setup() {
 
 	        if (typeof THREE === 'undefined') {
@@ -1448,17 +1472,27 @@ var units = Object.freeze({
 	            throw new RealityException('three.js most be newer than 85');
 	        }
 
-	        this.scene = new THREE.Scene();
+	        if (!this.scene) {
+	            this.scene = new THREE.Scene();
+	        }
 
-	        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, AU * 2);
-	        this.camera.position.z = AU;
+	        if (!this.camera) {
+	            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, this.scaled(AU * 2));
+	            this.camera.position.z = this.scaled(AU);
+	        }
 
-	        this.renderer = new THREE.WebGLRenderer();
-	        this.renderer.setSize(window.innerWidth, window.innerHeight);
+	        if (!this.renderer) {
+	            this.renderer = new THREE.WebGLRenderer();
+	            this.renderer.setSize(window.innerWidth, window.innerHeight);
+	        }
 
 	        for (const body of this.bodiesForSetup()) {
 	            body.render = new THREE.Mesh(
-	                new THREE.CubeGeometry(body.size.x, body.size.y, body.size.z),
+	                new THREE.CubeGeometry(
+	                    this.scaled(body.size.x),
+	                    this.scaled(body.size.y),
+	                    this.scaled(body.size.z)
+	                ),
 	                new THREE.MeshBasicMaterial({color: 0xff0000, wireframe: true}) //, side:THREE.BackSide
 	            );
 	            this.scene.add(body.render);
@@ -1474,9 +1508,9 @@ var units = Object.freeze({
 
 	        for (const thing of this.universe.bodies) {
 	            thing.render.position.set(
-	                thing.position.x,
-	                thing.position.y,
-	                thing.position.z
+	                this.scaled(thing.position.x),
+	                this.scaled(thing.position.y),
+	                this.scaled(thing.position.z)
 	            );
 	        }
 
